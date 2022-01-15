d40208a082634af1c615fa7010b35ae8cb54b025
Jenny NPC <jenny.nguyen935@my.tccd.edu>
2022-01-14 21:08:57 -0500

    Exercises for Methods Chapter

---
+++ methods/exercise4.rb
@@ -0,0 +1,13 @@
+=begin
+What will the following code print to the screen?
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+Answer:
+This code prints nil to the screen.
+=end
